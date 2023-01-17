193e58cf72481b87bdf78a9375da2ce27593fb9b
gahee99 <u1879@sookmyung.ac.kr>
2023-01-17 17:07:47 +0900

    정답

--- 인프런/Chapter[6]/부분집합구하기.py
+++ 인프런/Chapter[6]/부분집합구하기.py
@@ -1,18 +1,20 @@
 #자연수 N이 주어지면 1부터 N까지의 원소를 갖는 집합의 부분집합을 모두 출력하는 프로그램 작성
 import sys
 sys.stdin = open("인프런/Chapter[6]/input.txt",'r')
-n = int(input())
-N = [i for i in range(1, n+1)]
 
-def DFS(L):
-    if L > n :
-        for k in res:
-            print(k, end=' ')
+def DFS(v):
+    if v == n+1 : 
+        for i in range(1,n+1):
+            if ch[i] ==1:
+                print(i,end=' ')
         print()
-    else:
-        res.append(L)
-        DFS(L+1)
-        res.pop()
-        DFS(L+1)
-res=[]
+    else: 
+        ch[v]=1
+        DFS(v+1)
+        ch[v]=0
+        DFS(v+1)
+
+
+n = int(input())
+ch=[0]*(n+1)
 DFS(1)
