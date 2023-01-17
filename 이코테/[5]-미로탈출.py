f2645ea8f3f85d0a81805504dd5efc55c8019db4
gahee99 <u1879@sookmyung.ac.kr>
2023-01-17 16:18:36 +0900

    혼자 작성해보기

--- 이코테/[5]-미로탈출.py
+++ 이코테/[5]-미로탈출.py
@@ -9,26 +9,23 @@ for i in range(n):
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
-def bfs(x,y):
-    # 큐 라이브러리 사용
-    queue = deque()
-    queue.append((x,y))
-    while queue:
-        x, y = queue.popleft()
-        # 현재 위치에서 네방향 확인 
-        print("x,y " , x,y)
+def dfs(x,y):
+    que = deque()
+    que.append((x,y))
+
+    while que:
+        x,y = que.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            # 미로 찾기 공간을 벗어난 경우 무시 
-            if nx<0 or ny <0 or nx>=n or ny>=m:
+            if nx<0 or nx>=n or ny<0 or ny>=m:
                 continue
-            # 벽인 경우 무시
             if graph[nx][ny]==0:
                 continue
-            # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
             if graph[nx][ny]==1:
-                graph[nx][ny]= graph[x][y]+1
-                queue.append((nx,ny))
+                que.append((nx,ny))
+                graph[nx][ny] = graph[x][y]+1
     return graph[n-1][m-1]
-print(bfs(0,0))
+    
+
+print(dfs(0,0))
