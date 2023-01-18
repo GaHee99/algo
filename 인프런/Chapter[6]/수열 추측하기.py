6a1e8374669f739cbd3ef151f62a9204fc1bc8a2
gahee99 <u1879@sookmyung.ac.kr>
2023-01-18 20:37:09 +0900

    수열 추측하기 정답

--- 인프런/Chapter[6]/수열 추측하기.py
+++ 인프런/Chapter[6]/수열 추측하기.py
@@ -7,28 +7,20 @@ n,Sum = map(int,input().split())
 numbers = [0]*n
 ch=[0]*(n+1)
 mul = [1]*n
+for i in range(1,n):
+    mul[i]= mul[i-1]*(n-i)//(i)
 res= 0 
-def DFS(L):
-    global count
-    global res 
-    if L == n:
-        for k in range(n):
-            res += numbers[k]*mul[k]
-        if res == Sum:
-            for i in numbers:
-                print(i,end=' ')
-            sys.exit(0)
-        else:
-            res=0
+def DFS(L, sum):
+    if L == n and sum == Sum:
+        for x in numbers:
+            print(x, end =' ')
+        sys.exit(0)
     else:
-        for i in range(1,n+1):
+        for i in range(1, n+1):
             if ch[i]==0:
-                numbers[L]=i
                 ch[i]=1
-                DFS(L+1)
+                numbers[L]=i
+                DFS(L+1, sum+(numbers[L]*mul[L]))
                 ch[i]=0
-def multiple(n):
-    for i in range(1,n):
-        mul[i]= mul[i-1]*(n+1-i)//(i)
-multiple(n-1)
-DFS(0)
+
+DFS(0,0)
