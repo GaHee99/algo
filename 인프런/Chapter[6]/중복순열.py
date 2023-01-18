19fe29e38ea254d2afdcdcd47f780a95f7a73c60
gahee99 <u1879@sookmyung.ac.kr>
2023-01-18 19:55:38 +0900

    중복순열

---
+++ 인프런/Chapter[6]/중복순열.py
@@ -0,0 +1,20 @@
+import sys
+sys.stdin = open("인프런/Chapter[6]/input.txt",'r')
+
+n,m = map(int,input().split())
+li = [i for i in range(1,n+1)]
+res = [0]*m
+count=0
+def DFS(L):
+    global count
+    if L == m:
+        for i in res:
+            print(i, end= ' ')
+        count+=1
+        print()
+    else:
+        for i in range(1,n+1):
+            res[L]=i
+            DFS(L+1)
+DFS(0)
+print(count)
