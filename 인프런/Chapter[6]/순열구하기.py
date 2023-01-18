4a238692220bccf78a4c606b15d0ce2e84e2b906
gahee99 <u1879@sookmyung.ac.kr>
2023-01-18 20:14:45 +0900

    순열 구하기 완료

---
+++ 인프런/Chapter[6]/순열구하기.py
@@ -0,0 +1,25 @@
+import sys
+sys.stdin = open("인프런/Chapter[6]/input.txt",'r')
+
+n, m = map(int,input().split())
+res = [0]* m 
+ch= [0]*(n+1)
+count=0
+def DFS(L):
+    global count
+    if L==m:
+        for i in res:
+            print(i,end=' ')
+        count+=1
+        print()
+    else:
+        for i in range(1,n+1):
+            if ch[i]==0:
+                ch[i]=1
+                res[L]=i
+                DFS(L+1)
+                ch[i]=0
+
+
+DFS(0)
+print(count)
