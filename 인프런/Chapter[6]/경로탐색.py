1f221d48d51afa4b52ae4ef5d161a959b8fbc24e
gahee99 <u1879@sookmyung.ac.kr>
2023-01-19 16:54:14 +0900

    경로탐색 깨달은점 완료

--- 인프런/Chapter[6]/경로탐색.py
+++ 인프런/Chapter[6]/경로탐색.py
@@ -1,5 +1,10 @@
 import sys
 sys.stdin = open("인프런/Chapter[6]/input.txt",'r')
+# 경로탐색...
+# 인접해 있을 경우
+# 똑같이 DFS로 탐색을 하되, 갈 수 있는 경로는 인접 그래프를 통해서 파악한다..
+# DFS는 모든 그래프를 끝까지 탐색 할 수 있다는 것을 기억하자..
+# BFS는 최단거리를 기준으로 갈 수 있다! 
 def DFS(v):
     global cnt, path
     if v==n:
