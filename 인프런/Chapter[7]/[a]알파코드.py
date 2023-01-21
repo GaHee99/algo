354be01af7eaa9b9bac2cab18c84b48106836bbd
gahee99 <u1879@sookmyung.ac.kr>
2023-01-21 19:28:11 +0900

    알파코드 정답

--- 인프런/Chapter[7]/[a]알파코드.py
+++ 인프런/Chapter[7]/[a]알파코드.py
@@ -1,29 +1,26 @@
-
 import sys
 sys.stdin = open("인프런/Chapter[7]/input.txt")
 
 code = list(map(int,input()))
-n = len(code) # n이 종착점 
-code.insert(n,-1) # 마지막을 알림 , out of index 처리 
-res = [0]*(n+3) 
-cnt = 0 
-
-def DFS(L, P): 
-    global cnt
+n = len(code)
+code.insert(n,-1)
+res = [0]*(n+3)
+count = 0
+def DFS(L, P):
+    global count
     if L == n :
-        cnt+=1 
-        for j in range(P):
-            print(chr(res[j]+64),end='')
+        count+=1
+        for i in range(P):
+            print(chr(res[i]+64),end='')
         print()
     else:
         for i in range(1,27):
-            if code[L] == i :
+            if code[L]==i:
                 res[P]=i
-                DFS(L+1, P+1)
-            elif i >= 10 and code[L]== i//10 and code[L+1] == i%10:
+                DFS(L+1,P+1)
+            elif i >= 10 and code[L] == i //10 and code[L+1] == i%10:
                 res[P]=i
-                DFS(L+2, P+1) # P는 값을 집어 넣는 index이므로 ,, ,
-            
-
+                DFS(L+2,P+1)
+        
 DFS(0,0)
-print(cnt)
+print(count)
