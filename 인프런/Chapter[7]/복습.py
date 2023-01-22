7957bc934878108aef685b35a30e85fbd8e84c86
gahee99 <u1879@sookmyung.ac.kr>
2023-01-22 14:02:56 +0900

    알파코드 다시

--- 인프런/Chapter[7]/복습.py
+++ 인프런/Chapter[7]/복습.py
@@ -1,24 +1,29 @@
 import sys
 sys.stdin = open("인프런/Chapter[7]/input.txt")
 
-n = int(input())
-T = [0]
-P = [0]
-
-for i in range(n):
-    time, price = map(int,input().split())
-    T.append(time)
-    P.append(price)
-res = 0
-def DFS(day, price):
-    global res
-    if day == n+1:
-        if price>res:
-            res = price
+code = list(map(int,input()))
+n = len(code)
+res = [0]*(n+3)
+code.insert(n,-1)
+count=0
+def DFS(L, P):
+    global count
+    if L == n:
+        for i in range(P):
+            print(chr(res[i]+64),end='')
+        print()
+        count+=1
+     
     else:
-        if day+T[day]<=n+1:
-            DFS(day+T[day],price+P[day])
-        DFS(day+1, price)
-DFS(1,0)
-print(res)
+        for k in range(1,27):
+            if k == code[L]: # 한자리 수 일 경우 
+                res[P]=k
+                DFS(L+1,P+1)
+            elif k//10 == code[L] and k%10 == code[L+1]: # 두자리 수 일 경우
+                res[P] = k
+                DFS(L+2, P+1)
+
+
+DFS(0,0)
+print(count)
 
