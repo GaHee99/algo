14f542887b1fc36229a96f67f6d53fe67f02cd09
gahee99 <u1879@sookmyung.ac.kr>
2023-01-22 20:09:00 +0900

    왜 값이 복사가 될까?

---
+++ 인프런/Chapter[7]/안전영역.py
@@ -0,0 +1,46 @@
+import sys
+sys.stdin = open("인프런/Chapter[7]/input.txt")
+from collections import deque
+n = int(input())
+M = []
+S = set()
+for i in range(n):
+    List = list(map(int,input().split()))
+    M.append(List)
+    for i in List:
+        S.add(i)
+
+def BFS(max,Map):
+    dx = [0,0,1,-1]
+    dy = [1,-1,0,0]
+    que=deque()
+    count=0
+    for i in range(n):
+        for k in range(n):
+            if Map[i][k]<=max:
+                Map[i][k]=0
+    for i in range(n):
+        for k in range(n):
+            if Map[i][k]!=0:
+                count+=1
+                que.append((i,k))
+                Map[i][k]=0
+                while(que):
+                    x, y = que.popleft()
+                    for m in range(4):
+                        xx = dx[m]+ x 
+                        yy = dy[m]+ y
+                        if 0<=xx<n and 0<=yy<n and Map[xx][yy]!=0:
+                            Map[xx][yy]=0
+                            que.append((xx,yy))
+    return count
+
+
+res= []
+for i in S:
+    tmp = [] 
+    for k in M:
+        tmp.append(k)
+    BFS(i,tmp)
+    print(M)
+   
