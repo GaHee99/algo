61448fad446a0be38c6ff22558bf507015b6849e
gahee99 <u1879@sookmyung.ac.kr>
2023-01-22 14:15:27 +0900

    미로찾기 완료

--- 인프런/Chapter[7]/복습.py
+++ 인프런/Chapter[7]/복습.py
@@ -1,29 +1,28 @@
 import sys
 sys.stdin = open("인프런/Chapter[7]/input.txt")
-
-code = list(map(int,input()))
-n = len(code)
-res = [0]*(n+3)
-code.insert(n,-1)
-count=0
-def DFS(L, P):
-    global count
-    if L == n:
-        for i in range(P):
-            print(chr(res[i]+64),end='')
-        print()
-        count+=1
-     
-    else:
-        for k in range(1,27):
-            if k == code[L]: # 한자리 수 일 경우 
-                res[P]=k
-                DFS(L+1,P+1)
-            elif k//10 == code[L] and k%10 == code[L+1]: # 두자리 수 일 경우
-                res[P] = k
-                DFS(L+2, P+1)
-
-
-DFS(0,0)
-print(count)
-
+from collections import deque
+# 동빈이가 탈출 해야하는 최소한의 개수
+# 옆으로 넘어가는거는 1로 동일
+# BFS이용 가능 
+n, m = map(int,input().split())
+Map = []
+for i in range(n):
+    Map.append(list(map(int,input())))
+dx = [0,0,-1,1]
+dy = [1,-1,0,0]
+que = deque([(0,0)])
+Map[0][0] = 0
+distance = [[0]* m for _ in range(n)]
+distance[0][0]=1
+while que:
+    x, y = que.popleft()
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx<0 or nx >=n or ny<0 or ny>=m:
+            continue
+        if Map[nx][ny] == 1 :
+            distance[nx][ny]=distance[x][y]+1
+            que.append((nx,ny))
+            Map[nx][ny]=0
+print(distance[n-1][m-1])
