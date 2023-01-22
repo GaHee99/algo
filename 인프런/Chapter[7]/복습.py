20b6a44c0a5f800b70c2d6ef5a57a46ff1539a69
gahee99 <u1879@sookmyung.ac.kr>
2023-01-22 14:24:47 +0900

    특정 거리의 도시 찾기 완료

--- 인프런/Chapter[7]/복습.py
+++ 인프런/Chapter[7]/복습.py
@@ -1,28 +1,32 @@
 import sys
 sys.stdin = open("인프런/Chapter[7]/input.txt")
 from collections import deque
-# 동빈이가 탈출 해야하는 최소한의 개수
-# 옆으로 넘어가는거는 1로 동일
-# BFS이용 가능 
-n, m = map(int,input().split())
-Map = []
-for i in range(n):
-    Map.append(list(map(int,input())))
-dx = [0,0,-1,1]
-dy = [1,-1,0,0]
-que = deque([(0,0)])
-Map[0][0] = 0
-distance = [[0]* m for _ in range(n)]
-distance[0][0]=1
+
+# n : 노드, m: 간선 수, k = 거리, x : 시작 
+n,m,k,x = map(int,input().split())
+
+# 연결 돼있는 간선 받기 위한 그래프
+graph = [[] for _ in range(n+1)]
+
+# 거리를 위한 그래프 
+distance = [0]*(n+1)
+
+for _ in range(m):
+    start_node, end_node  = map(int,input().split())
+    graph[start_node].append(end_node)
+
+# 처음 시작 노드 넣고 
+que = deque([x])
 while que:
-    x, y = que.popleft()
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if nx<0 or nx >=n or ny<0 or ny>=m:
-            continue
-        if Map[nx][ny] == 1 :
-            distance[nx][ny]=distance[x][y]+1
-            que.append((nx,ny))
-            Map[nx][ny]=0
-print(distance[n-1][m-1])
+    current = que.popleft()
+    for next_node in graph[current]:
+        if distance[next_node]==0:
+            distance[next_node] = distance[current]+1
+            que.append(next_node)
+flg = False
+for index, dis in enumerate(distance):
+    if dis == k:
+        print(index)
+        flg = True
+if flg==False:
+    print(-1)
