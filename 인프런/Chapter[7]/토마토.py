8ea907a864d7d4839841f73079100411f881c1cc
GaHee99 <u1879@naver.com>
2023-01-26 09:43:04 +0900

    토마토 정답

--- 인프런/Chapter[7]/토마토.py
+++ 인프런/Chapter[7]/토마토.py
@@ -1,42 +1,36 @@
 import sys
-sys.stdin = open("인프런/Chapter[7]/input.txt")
 from collections import deque
-m,n = map(int,input().split())
-tomatoes = []
-for i in range(n):
-    tomatoes.append(list(map(int,input().split())))
-ripen= deque([])
-flag=True
-for i in range(n):
-    for k in range(m):
-        if tomatoes[i][k] == 1:
-            ripen.append((i,k))
-        if tomatoes[i][k] == -1:
-            flag = False
-dx = [0,0,1,-1]
-dy = [1,-1,0,0]
-
-L=0
-if flag == True and len(ripen)==0:
-    print(0)
-    sys.exit(0)
-
-while ripen:
-    l = len(ripen)
-    for _ in range(l):
-        x, y = ripen.popleft()
-        for a in range(4):
-            nx = dx[a] + x 
-            ny = dy[a] + y
-            if 0<=nx<n and 0<=ny<m:
-                if tomatoes[nx][ny] == 0:
-                    tomatoes[nx][ny]=1
-                    ripen.append((nx,ny))
-    L=L+1
-for b in tomatoes:
-    if 0 in b:
-        print(-1)
-        sys.exit(0)
-print(L-1)
-
-
+#sys.stdin=open("input.txt", "r")
+dx=[-1, 0, 1, 0]
+dy=[0, 1, 0, -1]
+n, m=map(int, input().split())
+board=[list(map(int, input().split())) for _ in range(m)]
+Q=deque()
+dis=[[0]*n for _ in range(m)]
+for i in range(m):
+    for j in range(n):
+        if board[i][j]==1:
+            Q.append((i, j))
+while Q:
+    x, y=Q.popleft()
+    for i in range(4):
+        nx=x+dx[i]
+        ny=y+dy[i]
+        if 0<=nx<m and 0<=ny<n and board[nx][ny]==0:
+            board[nx][ny]=1
+            dis[nx][ny]=dis[x][y]+1
+            Q.append((nx, ny))
+flag=1
+for i in range(m):
+    for j in range(n):
+        if board[i][j]==0:
+            flag=0
+result=0
+if flag==1:
+    for i in range(m):
+        for j in range(n):
+            if dis[i][j]>result:
+                result=dis[i][j]
+    print(result)
+else:
+    print(-1)
