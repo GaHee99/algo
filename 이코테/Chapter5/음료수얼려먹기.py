d5f4dea28566a8e94cecb8fd163a6ebbf8d4687e
iamrae <iamrae@swallaby.com>
2023-09-18 11:48:53 +0900

    음료수 얼려먹기 정답 풀이

--- 이코테/Chapter5/음료수얼려먹기.py
+++ 이코테/Chapter5/음료수얼려먹기.py
@@ -1,21 +1,22 @@
-def DFS(graph, x, y, N, M):
-    if 0<=x<=N-1 and 0<=y<=M-1 and graph[x][y] == 0:
-        graph[x][y] = 1 
-        DFS(graph, x+1, y, N, M)
-        DFS(graph, x-1, y, N, M)
-        DFS(graph, x, y+1, N, M)
-        DFS(graph, x, y-1, N, M)
-    else:
-        return 
-
 N, M = map(int,input().split())
 graph = []
 for i in range(N):
     graph.append(list(map(int,input())))
 count = 0 
+
+def DFS(graph, x, y):
+    if 0<=x<=N-1 and 0<=y<=M-1 and graph[x][y] == 0:
+        graph[x][y] = 1 
+        DFS(graph, x+1, y)
+        DFS(graph, x-1, y)
+        DFS(graph, x, y+1)
+        DFS(graph, x, y-1)
+    else:
+        return 
+    
 for i in range(N):
     for j in range(M):
         if graph[i][j] == 0:
             count+=1
-            DFS(graph, i, j , N,M)
+            DFS(graph, i, j)
 print(count)
