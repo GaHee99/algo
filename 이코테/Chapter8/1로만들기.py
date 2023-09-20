524cb63b329f79ce9fc177d645824786f34125c5
iamrae <iamrae@swallaby.com>
2023-09-20 15:47:09 +0900

    dp 개미전사

--- 이코테/Chapter8/1로만들기.py
+++ 이코테/Chapter8/1로만들기.py
@@ -1,7 +1,7 @@
 # 정수 x 입력받기 
 x = int(input())
 
-d = [0] * 30
+d = [[0] * 30]
 
 
 # 다이나믹 프로그래밍 보텀업
@@ -19,6 +19,6 @@ for i in range(2, x+1):
     
     if i%5 == 0:
         d[i] = min(d[i], d[i//5]+1)
-    print(i, d[i])
+        
 print(d)
 print(d[x])
